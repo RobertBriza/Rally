bb2eeda231db7b12b89e9e87b17fe0209e2afe1d
robertbriza <robert.briza@vivantis.cz>
2023-10-15 21:55:22 +0200

    Fix: deploy issues

--- deploy/php/entrypoint.sh
+++ deploy/php/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+until nc -z -v -w30 $MYSQL_HOST 3306; do
+  echo "Waiting for database connection..."
+  sleep 1
+done
+
 cd /var/www/html
 composer install
 php bin/console app:generate-config
